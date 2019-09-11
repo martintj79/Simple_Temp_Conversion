08753f978f2281b773f0b4002c9b261c65ac0965
Smash Reboot <martintj79@yahoo.com>
2019-09-10 20:32:12 -0500

    Initial Program
    Initial php code

---
+++ Simple_Temp_Conversion.php
@@ -0,0 +1,51 @@
+<?php
+$c = $f = '';
+
+if (isset ($_POST['f'])) $f = (sanitizeString($_POST['f']));
+if (isset ($_POST['c'])) $c = (sanitizeString($_POST['c']));
+
+if (is_numeric($f))
+{
+    $c   = intval((5/9) * ($f - 32));
+    $out = "$f &deg;f equals $c &deg;c";
+}
+elseif (is_numeric($c))
+{
+    $f   = intval((9/5) * ($c + 32));
+    $out = "$c &deg;c equals $f &deg;f";
+}
+else $out = "";
+
+echo <<<_END
+
+<html>
+    <head>
+        <title><font color="red">Temperature Converter</font>
+        </title>
+    </head>
+    <body>
+        <pre>
+            Enter either Fahrenheit or Celsius and click on Convert
+            
+            <b>$out</b>
+            <form method = "post" action="practice_convert.php">
+                Farenheit <input type="text" name="f" size="7">
+                Celcius   <input type="text" name="c" size="7">
+                          <input type="submit" value="Convert!">
+            </form>
+        </pre>
+    </body>
+</html>
+_END;
+
+function sanitizeString($var)
+{
+    if (get_magic_quotes_gpc())
+        $var = striipslashes($var);
+    $var = strip_tags($var);
+    $var = htmlentities($var);
+    return $var;
+}
+
+
+?>
